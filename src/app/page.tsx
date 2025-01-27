b8657735fd0098d1e1afe823d76b7a538dcc2575
yuribodo <yemotalara@gmail.com>
2025-01-26 22:48:57 -0400

    feat: add the components to the initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+import Features from '@/components/Features';
+import Pricing from '@/components/Pricing';
+import Reviews from '@/components/Reviews';
+import FAQ from '@/components/FAQ';
+import Footer from '@/components/Footer';
 
-const page = () => {
+const Page = () => {
   return (
-    <div>teste</div>
-  )
-}
+    <div className="min-h-screen bg-gray-950 text-gray-100">
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Reviews />
+      <FAQ />
+      <Footer/>
+    </div>
+  );
+};
 
-export default page
+export default Page;
