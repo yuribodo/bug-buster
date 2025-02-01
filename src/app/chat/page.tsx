4eff7b531b46e52a3ff1c6a5bf792fcf351cbb96
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-01-31 23:24:10 -0300

    feat: add sidebar component to chat page

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,9 +1,11 @@
-import ChatInput from "@/components/ChatInput";
+import ChatInput from '@/components/ChatInput';
+import ChatSideBar from '@/components/ChatSideBar';
 
 const page = () => {
   return (
     <div className="flex justify-center min-h-screen max-h-screen bg-zinc-950 text-gray-100">
       <ChatInput />
+      <ChatSideBar />
     </div>
   );
 };
