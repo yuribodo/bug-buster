ef99f45bf3dda5089577565b2dab256a4a49568d
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-02-11 11:35:44 -0300

    fix: solve window not defined server error

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -6,20 +6,28 @@ import { SendHorizonal } from 'lucide-react';
 const ChatInput = () => {
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
   const [prompt, setPrompt] = useState('');
+
   const handleInput = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setPrompt(e.target.value);
+
     if (textareaRef.current) {
       textareaRef.current.style.height = 'auto';
       const newHeight = Math.min(textareaRef.current.scrollHeight, 200);
       textareaRef.current.style.height = `${newHeight}px`;
     }
   };
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setPrompt('');
-    console.log(prompt);
   };
-  console.log('prompt:', prompt);
+  const windowWidthHandler = () => {
+    if (typeof window !== 'undefined') {
+      return window.innerWidth;
+    }
+  };
+  const windowWidth = windowWidthHandler();
+
   return (
     <label className="bg-zinc-800 flex flex-row self-end w-4/5 md:w-2/5 m-2 rounded-lg p-3">
       <form onSubmit={(e) => handleSubmit(e)} className="p-2 flex w-full">
@@ -35,7 +43,7 @@ const ChatInput = () => {
           className={` ${prompt ? 'hover:scale-110' : ''} disabled:opacity-30 justify-end`}
           disabled={!prompt}
         >
-          <SendHorizonal size={window.innerWidth > 1000 ? 32 : 25} />
+          <SendHorizonal size={windowWidth! > 1000 ? 32 : 25} />
         </button>
       </form>
     </label>
